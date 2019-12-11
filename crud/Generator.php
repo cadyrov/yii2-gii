934d8c5b09edfe2deb1c2c7ccfb2041254f1ede4
kot <cadyrov@gmail.com>
2019-12-11 16:19:34 +0500

    init

--- crud/Generator.php
+++ crud/Generator.php
@@ -151,27 +151,23 @@ class Generator extends \yii\gii\Generator
     public function generate()
     {
         $controllerFile = Yii::getAlias('@' . str_replace('\\', '/', ltrim($this->controllerClass, '\\')) . '.php');
-
         $files = [
             new CodeFile($controllerFile, $this->render('controller.php')),
         ];
-
         if (!empty($this->searchModelClass)) {
             $searchModel = Yii::getAlias('@' . str_replace('\\', '/', ltrim($this->searchModelClass, '\\') . '.php'));
             $files[] = new CodeFile($searchModel, $this->render('search.php'));
         }
-
         $viewPath = $this->getViewPath();
         $templatePath = $this->getTemplatePath() . '/views';
         foreach (scandir($templatePath) as $file) {
-            if (empty($this->searchModelClass)) {
+            if (empty($this->searchModelClass) && $file === '_search.php') {
                 continue;
             }
             if (is_file($templatePath . '/' . $file) && pathinfo($file, PATHINFO_EXTENSION) === 'php') {
                 $files[] = new CodeFile("$viewPath/$file", $this->render("views/$file"));
             }
         }
-
         return $files;
     }
 
